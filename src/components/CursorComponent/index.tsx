238358be8769a1825e954b5ef895990fed341de6
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-05-07 19:24:15 -0300

    refactor(CursorComponent):forwarding ref to cursor Component to use in Card component logic

--- src/components/CursorComponent/index.tsx
+++ src/components/CursorComponent/index.tsx
@@ -1,26 +1,8 @@
 import "@styles/_aula-05.scss"
-
-import { useEffect, useRef, useCallback} from "react";
-function CursorComponent() {
-    const cursorRef = useRef<HTMLDivElement>(null)
-    const mouseMoveHandler=useCallback((element:MouseEvent)=>{
-        element.preventDefault();
-        const target=cursorRef.current
-        if(!target)return
-        target.style.left = `${element.clientX}px` // Move the cursorComponent to the same X position of the cursor
-        target.style.top = `${element.clientY}px` // Move the cursorComponent to the same Y position of the cursor
-    },[cursorRef])
-    useEffect(() => {
-        const target = cursorRef.current
-        if(target){
-            window.onmousemove=(mouseMoveHandler)
-        }
-        return ()=>{
-            // cleanup function
-            window.onmousemove=null
-        }
-    }, [cursorRef,mouseMoveHandler]);
-    return ( <div id="cursor" ref={cursorRef}></div> );
-}
+import { forwardRef } from "react";
+const CursorComponent=forwardRef<HTMLDivElement>((_props,ref) =>{
+    
+    return ( <div id="cursor" ref={ref}></div> );
+})
 
 export default CursorComponent;
