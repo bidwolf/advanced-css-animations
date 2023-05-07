9292f748bcb9f0481edc502b66ec47d0e576d8ee
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-05-07 03:38:42 -0300

    chore(setup):starting project with vite scaffolding and create a css polygon with clip-path

---
+++ README.md
@@ -0,0 +1,4 @@
+# Awesome CSS Animations
+
+This repo is based on [this course](https://www.udemy.com/course/css-hover-animation-effects-from-beginners-to-expert).
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@styles/App.css'
+import Hexagon from './components/Hexagon'
+
+function App() {
+  return (
+    <>
+      <h1>teste</h1>
+      <Hexagon/>
+    </>
+  )
+}
+
+export default App
