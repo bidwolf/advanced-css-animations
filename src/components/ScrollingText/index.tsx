163ed9585185ba0e778e3fd1011a945a4e01c719
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-05-08 11:41:58 -0300

    feat(scrolling-text):create scrolling-text component and add it on app.tsx

---
+++ src/components/ScrollingText/index.tsx
@@ -0,0 +1,15 @@
+import "@styles/_aula-07.scss"
+function ScrollingText() {
+    return ( 
+        <section className="scrolling">
+        <div className="scrolling-container">
+            <h2 className="scrolling-title">This course is awesome to learn useful CSS ANIMATIONS</h2>
+        </div>
+        <div className="scrolling-container">
+            <h2 className="scrolling-title">This course is awesome to learn useful CSS ANIMATIONS</h2>
+        </div>
+        </section>
+    )
+}
+
+export default ScrollingText
