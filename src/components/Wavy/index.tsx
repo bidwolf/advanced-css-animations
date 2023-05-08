ed8d89776092fa2b2b2be08960c240145711acb9
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-05-08 09:52:00 -0300

    feat(wavy):create a component to render the wavy animation

---
+++ src/components/Wavy/index.tsx
@@ -0,0 +1,19 @@
+import "@styles/_aula-06.scss"
+function Wavy() {
+    return ( 
+    <div className="loader">
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+        <span className="wavy"></span>
+    </div> );
+}
+
+export default Wavy;
