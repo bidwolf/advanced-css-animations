b67b39eec176b85a9b995b267b9c6bb0e55bad43
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-05-11 13:30:10 -0300

    refactor(rootComponent):separate components to compose the root layout

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,79 +1,16 @@
-import "@styles/_home.scss"
-import { Outlet, Link, NavLink } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
 function Root() {
+    
     return (
         <>
-        <header>
-        <nav className="navbar">
-            <h1 id="course"><Link to={"/"} >Awesome-css-animation-course</Link></h1>
-            <ul className="navbar-list">
-                <li className="navbar-list-item">
-                    <NavLink
-                        to={"section/1"} 
-                        className={({ isActive, isPending }) =>
-                            `btn btn-link ${
-                                isActive
-                                    ? "active"
-                                    : isPending
-                                    ? "pending"
-                                    : ""
-                                }
-                        `
-                    }>Section 1
-                    </NavLink>
-                </li>
-                <li className="navbar-list-item">
-                    <NavLink to={"section/2"} 
-                        className={({ isActive, isPending }) =>
-                            `btn btn-link ${
-                                isActive
-                                    ? "active"
-                                    : isPending
-                                    ? "pending"
-                                    : ""
-                                }
-                        `
-                    }>Section 2
-                    </NavLink>
-                </li>
-                <li className="navbar-list-item">
-                    <NavLink to={"section/3"} 
-                        className={({ isActive, isPending }) =>
-                            `btn btn-link ${
-                                isActive
-                                    ? "active"
-                                    : isPending
-                                    ? "pending"
-                                    : ""
-                                }
-                        `
-                    }>Section 3
-                    </NavLink>
-                </li>
-                <li className="navbar-list-item">
-                    <NavLink to={"section/4"} 
-                        className={({ isActive, isPending }) =>
-                            `btn btn-link ${
-                                isActive
-                                    ? "active"
-                                    : isPending
-                                    ? "pending"
-                                    : ""
-                                }
-                        `
-                    }>Section 4
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-        </header>
+        <Navbar/>
         <main id="main">
             <Outlet/>
         </main>
-        <footer id="footer">
-            <h3>Created with &#128154; by Henrique de Paula Rodrigues</h3>
-        </footer>
+        <Footer/>
         </>
      );
 }
